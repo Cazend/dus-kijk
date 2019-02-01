1714a1ab1d30a8a05a48bcb7578c042b812df831
Cazend <47249452+Cazend@users.noreply.github.com>
2019-02-01 16:02:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,10 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
   if(command === "say")
-    const sayMessage = args.join(" ");
-    message.delete().catch(O_o=>{}); 
-    message.channel.send(sayMessage);
-  }
+    message.reply("Wow, het werkt!")
 }};
 
 client.login(process.env.BOT_TOKEN);
