1c5affa90e878f4955790dc14346168414a08c33
Cazend <47249452+Cazend@users.noreply.github.com>
2019-02-01 16:14:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,41 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-  if(command === "say")
-    message.reply("Wow, het werkt!")
-}};
+const config = require("./config.json");
+
+client.on("ready", () => {
+  console.log(`Bot has succesfully started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+  client.user.setActivity(`with Blackouts | .help`);
+});
+
+client.on("guildCreate", guild => {
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setActivity(`with Blackouts | .help`);
+});
+
+client.on("guildDelete", guild => {
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`with Blackouts | .help`);
+});
+
+
+client.on("message", async message => {
+
+  
+  if(message.author.bot) return;
+  
+
+  if(message.content.indexOf(config.prefix) !== 0) return;
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+
+  if(command === "ping") {
+    const m = await message.channel.send("Ping?")
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  }
+
+});
 
 client.login(process.env.BOT_TOKEN);
